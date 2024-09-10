7ad00f6454e92fff3242af76dead85e3a07ba190
Akraja712 <ashokkumar7hitter@gmail.com>
2024-09-10 18:03:43 +0530

    deduction.php

---
+++ admin_v1/api/deduction.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+
+$sql = "UPDATE withdrawals SET amount = amount * 0.8 WHERE status = 0";
+$db->sql($sql);
