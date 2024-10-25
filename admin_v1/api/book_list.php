84faad03c36cf3b7ce9ab5a570bd85e07f01a422
Akraja712 <ashokkumar7hitter@gmail.com>
2024-10-25 10:07:02 +0530

    book_list.php

---
+++ admin_v1/api/book_list.php
@@ -0,0 +1,29 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+$sql = "SELECT * FROM `books` ORDER BY RAND() LIMIT 1";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+
+if ($num >= 1) {
+    $response['success'] = true;
+    $response['message'] = "Book Listed Successfully";
+    $response['data'] = $res;
+    echo json_encode($response);
+} else {
+    $response['success'] = false;
+    $response['message'] = "Book Not Found";
+    echo json_encode($response);
+}
